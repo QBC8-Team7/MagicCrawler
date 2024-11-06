8ea036d629ca9275bfb5bfb2bc3a0fd944c084f8
MehdiMomeni <mehdimomeni@Meiti.local>
2024-11-07 02:28:11 +0330

    feat: handle gracefull shutdiwn

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/QBC8-Team7/MagicCrawler/config"
 	"github.com/QBC8-Team7/MagicCrawler/internal/server"
@@ -14,5 +19,24 @@ func main() {
 	}
 
 	s := server.NewServer(cfg)
-	s.Serve()
+
+	go func() {
+		s.Serve()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+	log.Println("Shutting down bot...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	s.Bot.Stop()
+
+	<-ctx.Done()
+
+	s.Logger.Info("Bot exited gracefully.")
+
 }
