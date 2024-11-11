7d98897e1bfc30e5bcdb076e984bc529a32d897e
MehdiMomeni <mehdimomeni@Meiti.local>
2024-11-11 08:25:15 +0330

    feat: add some handler for test

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -9,5 +9,13 @@ func (s *Server) MapHandlers(e *echo.Echo) error {
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(200, map[string]string{"status": "very OK"})
 	})
+
+	e.GET("/ads", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{"status": "ads very OK"})
+	})
+
+	e.GET("/search", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{"status": "search very OK"})
+	})
 	return nil
 }
