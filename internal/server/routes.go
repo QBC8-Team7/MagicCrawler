662f7a632aff09f28db88d3a50a163bd678e9cf2
Mohammad Zareabidaki <mohammadzarea.bidaki@gmail.com>
2024-11-15 11:16:30 +0330

    add api/v1 prefix as base url

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,30 +5,31 @@ import (
 )
 
 func registerRoutes(e *echo.Echo, s *Server) {
-	s.router.GET("/healthz", healthCheck)
+	apiV1 := e.Group("/api/v1")
 
-	adGroup := e.Group("/ad")
+	apiV1.GET("/healthz", healthCheck)
+
+	adGroup := apiV1.Group("/ad")
 	adGroup.DELETE("/:adID", s.deleteAdByID)
 	adGroup.GET("/search", s.searchAds)
 	adGroup.GET("/:adID", s.getAdById)
 	adGroup.GET("", s.getAllAds)
 	adGroup.POST("", s.createAd)
 
-	userGroup := e.Group("/user")
+	userGroup := apiV1.Group("/user")
 	userGroup.GET("/ad", s.getUsersAds)
 	userGroup.POST("/favorite/:adID", s.createUserFavoriteAd)
 	userGroup.DELETE("/favorite/:adID", s.deleteUserFavoriteAd)
 	userGroup.GET("/favorite", s.getUserFavoriteAds)
 	userGroup.GET("", s.getUserInfo)
 
-	priceGroup := e.Group("/price")
+	priceGroup := apiV1.Group("/price")
 	priceGroup.GET("/:adID/all", s.getAdsAllPrices)
 	priceGroup.GET("/:adID", s.getAdsLatestPrice)
 	priceGroup.POST("", s.setPriceOnAd)
 
-	pictureGroup := e.Group("/picture")
+	pictureGroup := apiV1.Group("/picture")
 	pictureGroup.GET("/:adID", s.getAdPictures)
 	pictureGroup.DELETE("/:pictureID", s.deletePicture)
 	pictureGroup.POST("", s.createAdPicture)
-
 }
