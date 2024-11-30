7f78202cb7b920c22eb3f88b8fc306e364c8819a
Mohammad Zareabidaki <mohammadzarea.bidaki@gmail.com>
2024-11-30 17:56:31 +0330

    add kubernetes resources

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" == "app" ]; then
   echo "Starting app..."
-  ./app
+  exec ./app $2 $3
 elif [ "$1" == "crawler" ]; then
   echo "Starting crawler..."
-  ./crawler
+  exec ./crawler $2 $3
 else
   echo "Usage: $0 {app|crawler}"
   exit 1
